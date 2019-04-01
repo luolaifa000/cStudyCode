2b9bed2d473986fc89329ab95931fea6bada54bd
luolaifa000 <18510023526@163.com>
2019-04-02 00:00:03 +0800

    hashTable

--- hashTable.c
+++ hashTable.c
@@ -9,6 +9,7 @@ typedef struct {
     char *key;
     void *next;
     void *pre;
+    char *value;
 } hashNode;
 
 typedef struct {
@@ -26,9 +27,13 @@ int hashFunction(char *key, int size);
 
 int hashTableInsert(hashTable *hash, hashNode *node);
 
+char* hashTableFind(hashTable *hash, char *key);
+
+int hashTableDelete(hashTable *hash, char *key);
+
 void *defMalloc(int size);
 
-hashNode* createNode(const char *string);
+hashNode* createNode(const char *string, const char *value);
 
 void printfHashTable(hashTable *hash);
 
@@ -47,13 +52,19 @@ int main()
     int i = 0;
     for (i; i<5 ; ++i)
     {
-        hashNode *temp = createNode(arr[i]);
+        hashNode *temp = createNode(arr[i], arr[i]);
         hashTableInsert(hash, temp);
     }
 
     //遍历数据打印一下
     printfHashTable(hash);
-    
+
+    char *find = hashTableFind(hash, "dd");
+    printf("find:%s\n", find);
+
+
+    hashTableDelete(hash, "java");
+    printfHashTable(hash);
     free(hash->bucket);
     free(hash);
     return 0;
@@ -68,7 +79,7 @@ void printfHashTable(hashTable *hash)
     while(k++ < hash->size){
         /* code */
         if (hash->bucket[k]) {
-            printf("index:%d,key:%s\n",hash->bucket[k]->index,hash->bucket[k]->key);
+            printf("index:%d,key:%s,value:%s\n",hash->bucket[k]->index,hash->bucket[k]->key,hash->bucket[k]->value);
             p = hash->bucket[k];
             while(p->next) {
                 printf("index:%d,key:%s\n",((hashNode *)(p->next))->index,((hashNode *)(p->next))->key);
@@ -86,28 +97,63 @@ int hashTableInsert(hashTable *hash, hashNode *node)
     int index = (*((hashHandler) hash->func))(node->key, hash->size);
     node->index = index;
     printf("index:%d\n", index);
-    hashNode *p = hash->bucket[index];
-    if (p) {
+    hashNode **p = &hash->bucket[index];
+    if (*p) {
+        if (strcmp(node->key, (*p)->key) == 0) {
+                return 1;
+        }
         //地址冲突，找到链表尾节点在后面插入
-        while(p) {
-            if (strcmp(node->key, p->key) == 0) {
+        while((*p)->next) {
+            if (strcmp(node->key, (*p)->key) == 0) {
                 return 1;
             }
-            if (strcmp(node->key, p->key) < 0) {
-                node->next = p;
-
-                
-            }
-            p = (hashNode *) p->next;
+            (*p) = (hashNode *) (*p)->next;
         }
-        p = node;
+        (*p)->next = node;
 
     } else {
-        p = node;
+        (*p) = node;
     }
-
     hash->countNode++;
-    
+    return 0;
+}
+
+char* hashTableFind(hashTable *hash, char *key)
+{
+    int index = (*((hashHandler) hash->func))(key, hash->size);
+    if (hash->bucket[index]) {
+        hashNode *p = hash->bucket[index];
+        while(p) {
+            if (strcmp(key, p->key) == 0) {
+                return p->value;
+            }
+            p = p->next;
+        }
+    } 
+    return NULL;
+}
+
+
+int hashTableDelete(hashTable *hash, char *key)
+{
+    int index = (*((hashHandler) hash->func))(key, hash->size);
+    if (hash->bucket[index]) {
+        hashNode **p = &hash->bucket[index];
+        hashNode *p_pre;
+        while(*p) {
+            if (strcmp(key, (*p)->key) == 0) {
+                if (!p_pre) {
+                    *p = NULL;
+                } else {
+                    p_pre->next = *p;
+                }
+                
+                return 1;
+            }
+            p_pre = *p;
+            *p = (*p)->next;
+        }
+    } 
     return 0;
 }
 
@@ -130,12 +176,13 @@ void *defMalloc(int size)
 }
 
 
-hashNode* createNode(const char *string)
+hashNode* createNode(const char *string, const char *value)
 {
     hashNode *temp;
     temp = (hashNode *) defMalloc(sizeof(hashNode));
 
     temp->key = (char *)string;
+    temp->value = (char *)value;
     temp->index = 0;
     temp->next = NULL;
     temp->pre = NULL;
